30b9cae298cf5c9cd9321843529be4a1163dc95a
xflydan <nrzadty@gmail.com>
2023-12-02 04:33:50 +0700

    DanteMusic

---
+++ .github/README.md
@@ -0,0 +1,94 @@
+ <h1 align="center">
+  <b>🤖 𝗭𝗘𝗨𝗫 𝗥𝗢𝗕𝗢𝗧 🤖</b>
+ </h1>
+<p align="center">
+<a href="https://telegram.me/ZeuxXRobot"><img src="https://img.shields.io/badge/-🤖𝗭𝗘𝗨𝗫%20𝗥𝗢𝗕𝗢𝗧🤖-Grey.svg?style=for-the-badge&logo=Telegram"></a>
+</p1>
+  
+
+<p align="center">
+  <img src="https://telegra.ph/file/3248bb49c94172c834427.jpg" alt="Dante Logo">
+</p>
+
+----
+
+<p align="center">
+<b>𝗗𝗘𝗣𝗟𝗢𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗𝗦</b>
+</p>
+
+<h3 align="center">
+    ─「 ᴅᴇᴩʟᴏʏ ᴏɴ ᴠᴘs/ʟᴏᴄᴀʟ 」─
+</h3>
+
+<details>
+<summary><h3>
+- <b> ᴠᴘs/ʟᴏᴄᴀʟ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ ᴍᴇᴛʜᴏᴅ </b>
+</h3></summary>
+
+- Upgrade and Update by :
+`sudo apt-get update && sudo apt-get upgrade -y`
+- Install required packages by :
+`sudo apt-get install python3-pip ffmpeg -y`
+- Install pip by :
+`sudo pip3 install -U pip`
+- Install Node by :
+`curl -fssL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install nodejs -y && npm i -g npm`
+- Clone the repository by :
+`git clone https://github.com/xflydan/DanteMusic && cd DanteMusic`
+- Install the requirements:      
+`pip3 install -U -r requirements.txt`
+- Create .env  with sample.env by :
+`cp sample.env .env`
+- Editing Vars by :
+`vi .env`
+`Press I button on keyboard to start editing.`
+`Press Ctrl + C  once you are done with editing vars and type :wq to save .env or :qa to exit editing.`
+- Finally Run Dante Music Bot by :
+`bash start`
+
+
+</details>
+
+<details>
+<summary><h3>
+- <b> ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ </b>
+</h3></summary>
+
+> Anda memerlukan API_ID & API_HASH untuk menghasilkan sesi telethon. ambil APP ID dan API Hash di my.telegram.org
+<h4> Generate Session via Telegram StringGen Bot: </h4>    
+<p><a href="https://t.me/dantestringbot"><img src="https://img.shields.io/badge/TG%20String%20Gen%20Bot-blueviolet?style=for-the-badge&logo=appveyor" width="200""/></a></p>
+    
+</details>
+━━━━━━━━━━━━━━━━━━━━
+
+
+
+<h3 align="center">
+    ─「 sᴜᴩᴩᴏʀᴛ 」─
+</h3>
+
+<p align="center">
+<a href="https://telegram.me/dansupport"><img src="https://img.shields.io/badge/-Support%20Group-blue.svg?style=for-the-badge&logo=Telegram"></a>
+</p>
+<p align="center">
+<a href="https://telegram.me/danteprojext"><img src="https://img.shields.io/badge/-Support%20Channel-blue.svg?style=for-the-badge&logo=Telegram"></a>
+</p>
+
+━━━━━━━━━━━━━━━━━━━━
+
+<h3 align="center">
+    ─「 ᴄʀᴇᴅɪᴛs 」─
+</h3>
+
+Special thanks to these amazing projects/people which/who help power Yukki Music Bot:
+
+- [Pyrogram](https://github.com/pyrogram/pyrogram)
+- [Py-Tgcalls](https://github.com/pytgcalls/pytgcalls)
+- [CallsMusic Team](https://github.com/Callsmusic)
+- [TheHamkerCat](https://github.com/TheHamkerCat)
+- [Aarav Arora](https://github.com/axrav)
+
+> Made with 💕 by [@dansupport](https://t.me/dansupport).   
+ 
+━━━━━━━━━━━━━━━━━━━━
+
---
+++ DanteMusic/core/bot.py
@@ -0,0 +1,74 @@
+#
+# Copyright (C) 2023-2024 by xflydan@Github, < https://github.com/xflydan >.
+#
+# This file is part of < https://github.com/xflydan/music > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/xflydan/music/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import sys
+
+from pyrogram import Client
+from pyrogram.types import BotCommand
+
+import config
+
+from ..logging import LOGGER
+
+
+class DanteMusic(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"🤖 MENGAKTIFKAN BOT")
+        super().__init__(
+            "DanteMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+        )
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        try:
+            await self.send_message(
+                config.LOG_GROUP_ID, "🤖 BOT AKTIF YA KONTOL"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot gagal mengakses Grup log. Pastikan Anda telah menambahkan bot ke saluran log Anda dan dipromosikan sebagai admin!"
+            )
+            sys.exit()
+        if config.SET_CMDS == str(True):
+            try:
+                await self.set_bot_commands(
+                    [
+                        BotCommand("ping", "Periksa apakah bot itu hidup atau mati"),
+                        BotCommand("play", "Mulai memutar lagu yang diminta"),
+                        BotCommand("skip", "Pindah ke trek berikutnya dalam antrian"),
+                        BotCommand("pause", "Jeda lagu yang sedang diputar"),
+                        BotCommand("resume", "Lanjutkan lagu yang dijeda"),
+                        BotCommand("end", "Hapus antrian dan tinggalkan obrolan suara"),
+                        BotCommand("shuffle", "Mengacak daftar putar antrean secara acak."),
+                        BotCommand("playmode", "Memungkinkan Anda mengubah mode putar default untuk obrolan Anda"),
+                        BotCommand("settings", "Buka pengaturan bot musik untuk obrolan Anda.")
+                        ]
+                    )
+            except:
+                pass
+        else:
+            pass
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Harap promosikan Bot sebagai Admin di Grup Logger"
+            )
+            sys.exit()
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"🤖 BOT MUSIC DIAKTIFKAN SEBAGAI {self.name}")
+        
